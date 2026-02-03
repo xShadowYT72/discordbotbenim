65891b93c1142a0616c60459272e1420e6bd6713
xShadowYT72 <yagaoglu120@gmail.com>
2026-02-03 23:49:28 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} olarak giriş yapıldı!`);
+});
+
+client.on('messageCreate', msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+    msg.reply('Aleykümselam, botun 7/24 aktif!');
+  }
+});
+
+client.login(process.env.TOKEN);
